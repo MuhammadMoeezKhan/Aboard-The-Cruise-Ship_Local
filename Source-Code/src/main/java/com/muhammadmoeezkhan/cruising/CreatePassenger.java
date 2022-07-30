9829d3e57dd494115c704d7b95e1b8a2abaaa15e
Muhammad Moeez Khan <85444389+MuhammadMoeezKhan@users.noreply.github.com>
2022-07-30 00:37:24 -0400

    Add files via upload

---
+++ Source-Code/src/main/java/com/muhammadmoeezkhan/cruising/CreatePassenger.java
@@ -0,0 +1,45 @@
+package com.muhammadmoeezkhan.cruising;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.muhammadmoeezkhan.cruising.database.DBHelper;
+import com.muhammadmoeezkhan.cruising.database.Passenger;
+import com.muhammadmoeezkhan.cruising.databinding.ActivityCreatePassengerBinding;
+
+public class CreatePassenger extends AppCompatActivity {
+
+    private ActivityCreatePassengerBinding binding;
+
+    // Listener
+    private View.OnClickListener button_create_passenger_clickListener = new View.OnClickListener(){
+        @Override
+        public void onClick(View view) {
+            String firstName = binding.edittextFirstName.getText().toString();
+            String lastName = binding.edittextLastName.getText().toString();
+            int passportNumber = Integer.valueOf(binding.edittextPassport.getText().toString());
+            String birthdate = binding.edittextBirthdate.getText().toString();
+            String city = binding.edittextCity.getText().toString();
+            String state = binding.edittextState.getText().toString();
+
+            Passenger p = new Passenger(firstName, lastName, passportNumber, birthdate, city, state);
+            DBHelper helper = DBHelper.getInstance(CreatePassenger.this);
+
+            long result = helper.insertPassenger(p);
+            if(result>=0){
+                Toast.makeText(CreatePassenger.this, "Success", Toast.LENGTH_SHORT).show();
+            }
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityCreatePassengerBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        binding.buttonCreatePassenger.setOnClickListener(button_create_passenger_clickListener);
+    }
+}
---
+++ Source-Code/src/main/java/com/muhammadmoeezkhan/cruising/database/Passenger.java
@@ -0,0 +1,68 @@
+package com.muhammadmoeezkhan.cruising.database;
+
+public class Passenger {
+
+    private int id;
+    private String firstName;
+    private String lastName;
+    private int passportNumber;
+    private String birthdate;
+    private String city;
+    private String state;
+
+    public Passenger(int id, String firstName, String lastName, int passportNumber, String birthdate, String city, String state) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.passportNumber = passportNumber;
+        this.birthdate = birthdate;
+        this.city = city;
+        this.state = state;
+    }
+
+    public Passenger(String firstName, String lastName, int passportNumber, String birthdate, String city, String state) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.passportNumber = passportNumber;
+        this.birthdate = birthdate;
+        this.city = city;
+        this.state = state;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public int getPassportNumber() {
+        return passportNumber;
+    }
+
+    public String getBirthdate() {
+        return birthdate;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    @Override
+    public String toString() {
+        return  "Passenger ID = " + id + "\r\n" +
+                "First Name = '" + firstName + "\r\n" +
+                "Last Name = '" + lastName + "\r\n" +
+                "Passport Number = " + passportNumber
+                ;
+    }
+}
